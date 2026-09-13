8a85f4a226d8940377992c2d3667553df0db2fd7
Kien Ha <kienhgph50150@gmail.com>
2026-09-13 22:50:44 +0700

    feat(frontend): add state management

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,22 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { Provider } from 'react-redux'
 
-createRoot(document.getElementById('root')!).render(
-  <App />
+import App from '@/App'
+import { store } from '@/store'
+
+import './index.css'
+
+const rootElement = document.getElementById('root')
+
+if (!rootElement) {
+  throw new Error('Root element not found')
+}
+
+createRoot(rootElement).render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>,
 )
